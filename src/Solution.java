2e135df70a5385b44c0bed73a216895a75788e08
gabrielbabbler <gabriel.babler@sensedia.com>
2022-01-12 22:52:59 -0300

    solution of javaMap

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,30 @@
-public class Solution {
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh) {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        in.nextLine();
+
+        Map<String, Integer> phoneBook = new HashMap<>();
+
+        for(int i=0;i<n;i++) {
+            String name = in.nextLine();
+            int phone = in.nextInt();
+
+            phoneBook.put(name, phone);
+            in.nextLine();
+        }
+        while(in.hasNext()) {
+            String s = in.nextLine();
+
+            if(phoneBook.containsKey(s)){
+                System.out.println(s+"="+phoneBook.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+    }
 }
