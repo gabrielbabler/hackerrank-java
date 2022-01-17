407771612e5ba43b90bbd1dd9897a5a0afc1fc9a
gabrielbabbler <gabriel.babler@sensedia.com>
2022-01-16 22:55:18 -0300

    solution of javaSubstringComparisons

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class Solution {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        java.util.List<String> substrings = new java.util.ArrayList<>();
+
+        int i = 0;
+        do {
+            String substring = s.substring(i, k + i);
+            substrings.add(substring);
+            i++;
+        } while (s.length() >= k + i);
+
+        substrings.sort(java.util.Comparator.naturalOrder());
+
+        smallest = substrings.get(0);
+        largest = substrings.get(substrings.size() - 1);
+
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
 }
