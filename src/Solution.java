1d2b53e7a65e5f7af02b5a30b4342373ea888d82
gabrielbabbler <gabriel.babler@sensedia.com>
2022-01-16 23:01:39 -0300

    solution of javaStringReverse

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,24 @@
+import java.io.*;
+import java.util.*;
+
 public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        String[] splitWord = a.split("");
+
+        boolean isPalindrome = true;
+
+        for(int i=0; i<splitWord.length; i++) {
+            if (!splitWord[i].equals(splitWord[splitWord.length - i - 1])) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println(isPalindrome ? "Yes" : "No");
+    }
 }
